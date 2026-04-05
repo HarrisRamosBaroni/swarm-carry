05d8d7629d37f005700ecdf2aa93f215c8bf1d60
AndrewGS9000 <146119979+AndrewGS9000@users.noreply.github.com>
2026-04-05 15:48:17 +0200

    Added Dr cap code. Works about as well as mr cap, since (I assume) both algorithms suppose the robots are attached to load object, which they here aren't. Our factor grpah needs to, instead, generate control inputs for each robot, not a single one for everyone, or else robots pushing always go slower than those in front, so keeping formation intact is impossible. To run this DRCAP, follow roughly same instructions as MRCAP, i tried my best to keep the format identical. It can be run from experiments/centralised_drcap_fg/run_experiment.py, and has the same optional arguments as MRCAP. Note the current DRCAP script is in its centralised from, not in the distributed form. It's still different to MRCAP as it has a few more factors and nodes. (This current script has a lot of comments and print statements, I will clean them up eventually but right now I just want to save a version that works)

--- swarmlib/controllers/__init__.py
+++ swarmlib/controllers/__init__.py
@@ -5,5 +5,6 @@ Controllers for multi-robot payload transport.
 from .base_controller import BaseController
 from .centralized_mpc import CentralizedMPC
 from .mrcap_controller import MRCapController
+from .drcap_centralised_controller import DRCapController
 
-__all__ = ['BaseController', 'CentralizedMPC', 'MRCapController']
+__all__ = ['BaseController', 'CentralizedMPC', 'MRCapController', 'DRCapController']
