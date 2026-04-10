9c8f90750d0ef160813172335159f8183cf25dc4
wuchangji <changji.wu@elephantrobotics.com>
2026-04-11 01:00:55 +0800

    GPIO Read Test - Working

--- swarmlib/sensors/force/test.py
+++ swarmlib/sensors/force/test.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
-PIN = 17
+PIN = 11
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
     while True:
