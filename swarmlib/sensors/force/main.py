510ce59937fbe83a17996e35f3938f6d0a2a1722
wuchangji <changji.wu@elephantrobotics.com>
2026-04-12 21:47:56 +0800

    Fix I2C Bus Arg Pass

--- swarmlib/sensors/force/main.py
+++ swarmlib/sensors/force/main.py
@@ -5,20 +5,26 @@ from qwiic_nau7802 import QwiicNAU7802
 bus = 2
 driver = I2CDriver(bus)
 
-scale = QwiicNAU7802(driver)
-scale.begin()
+scale = QwiicNAU7802(i2c_driver=driver)
 try:
     if scale.is_connected() == False:
         print("Not connected")
-    if not scale.begin():
-        print("NAU7802 init failed")
+    print("12a", scale.is_connected())
+    print("123a", scale._i2c.isDeviceConnected(0x2a))
+
+    print(scale.begin())
+    if False:
+        pass
+#    if not scale.begin():
+#        print("NAU7802 init failed")
     else:
 #        tare = scale.get_average(samples=10, timeout_ms=2000)
 #        scale.set_zero_offset(tare)
         scale.set_calibration_factor(1.0)  # counts per unit (adjust)
 
         while True:
-            w = scale.get_weight(allow_negative=True, samples=1, timeout_ms=500)
+#            w = scale.get_weight(allow_negative=True, samples=1, timeout_ms=500)
+            w = scale.get_reading()
             print(f"Weight: {w:.3f}")
             time.sleep(0.5)
 except KeyboardInterrupt:
