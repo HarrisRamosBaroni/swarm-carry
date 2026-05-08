aeed964c8f3ed890ee8816bc3e4599b5db67f93f
harris <yuopres@gmail.com>
2026-05-08 14:47:11 +0100

    fix: improve import message for gtsam fail

--- swarmlib/controllers/__init__.py
+++ swarmlib/controllers/__init__.py
@@ -14,8 +14,23 @@ try:
     from .force_centralised_controller import ForceCentralisedController
     from .force_centralised_controller_cvel import ForceCentralisedControllerCVel
     from .forceless_centralised_controller import ForcelessCentralisedControllerCVel
-except ImportError:
-    pass
+except ImportError as _gtsam_err:
+    _msg = (
+        f"gtsam-dependent controllers are not available on this platform "
+        f"({_gtsam_err}). Install gtsam (https://gtsam.org) to use "
+        f"MRCapController, DRCapController, ForceCentralisedController, "
+        f"ForceCentralisedControllerCVel, or ForcelessCentralisedControllerCVel."
+    )
+
+    class _GtsamMissing:
+        def __init__(self, *args, **kwargs):
+            raise ImportError(_msg)
+
+    MRCapController = type("MRCapController", (_GtsamMissing,), {})
+    DRCapController = type("DRCapController", (_GtsamMissing,), {})
+    ForceCentralisedController = type("ForceCentralisedController", (_GtsamMissing,), {})
+    ForceCentralisedControllerCVel = type("ForceCentralisedControllerCVel", (_GtsamMissing,), {})
+    ForcelessCentralisedControllerCVel = type("ForcelessCentralisedControllerCVel", (_GtsamMissing,), {})
 
 __all__ = ['BaseController', 'CentroidEstimator', 'MRCapController',
            'DRCapController', 'DRCapDistributedController',
